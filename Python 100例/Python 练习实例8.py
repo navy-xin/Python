0213a28842d738de3fdb7887a933619e7a405ef1
navy-xin <65326278+navy-xin@users.noreply.github.com>
2020-12-11 17:18:05 +0800

    Default Changelist

---
+++ Python 100例/Python 练习实例8.py
@@ -0,0 +1,11 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2020/12/11 16:39
+# @Author : navy
+# @File : Python 练习实例8.py
+# @Software: PyCharm
+
+
+for i in range(1,10):
+    print()
+    for j in range(1, i+1):
+        print ("%d * %d = %d  " % (i,j,i*j), end=" ")
